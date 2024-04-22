eb748dca0ed0247480a967eaf5ee6b426efab441
Emmanuel <rodrigofemi@gmail.com>
2024-04-22 11:27:46 +0100

    Interactve Card

--- script.js
+++ script.js
@@ -14,51 +14,52 @@ const cvc = document.body.querySelector("#g");
 
 const submit = document.body.querySelector("#submit");
 
-const complete = document.getElementById("comp")
+const contiune = document.body.querySelector("#det");
+const complete = document.body.querySelector("#complete")
 
 submit.addEventListener("click", () => {
-    if (name.value === ""){
-        dMessage.textContent = `Input box cannot be empty`;
-        dMessage.style.color = "hsl(0, 100%, 66%)";
-        name.style.border = `1px solid hsl(0, 100%, 66%)`
-    }
+    // if (name.value === ""){
+    //     dMessage.textContent = `Input box cannot be empty`;
+    //     dMessage.style.color = "hsl(0, 100%, 66%)";
+    //     name.style.border = `1px solid hsl(0, 100%, 66%)`
+    // }
 
-    if (number.value === ""){
-        pMessage.textContent = `Wrong format, number only`;
-        pMessage.style.color = "hsl(0, 100%, 66%)";
-        number.style.border = `1px solid hsl(0, 100%, 66%)`
-    }
+    // if (number.value === ""){
+    //     pMessage.textContent = `Wrong format, number only`;
+    //     pMessage.style.color = "hsl(0, 100%, 66%)";
+    //     number.style.border = `1px solid hsl(0, 100%, 66%)`
+    // }
 
-    if (month.value === ""){
-        display.textContent = `Can't be blank`;
-        display.style.color = "hsl(0, 100%, 66%)";
-        month.style.border = `1px solid hsl(0, 100%, 66%)`
-    }
+    // if (month.value === ""){
+    //     display.textContent = `Can't be blank`;
+    //     display.style.color = "hsl(0, 100%, 66%)";
+    //     month.style.border = `1px solid hsl(0, 100%, 66%)`
+    // }
 
-    if (year.value === ""){
-        display.textContent = `Can't be blank`;
-        display.style.color = "hsl(0, 100%, 66%)";
-        year.style.border = `1px solid hsl(0, 100%, 66%)`
-    }
+    // if (year.value === ""){
+    //     display.textContent = `Can't be blank`;
+    //     display.style.color = "hsl(0, 100%, 66%)";
+    //     year.style.border = `1px solid hsl(0, 100%, 66%)`
+    // }
     
-    if (cvc.value === ""){
-        cDisplay.textContent = `Can't be blank`;
-        cDisplay.style.color = "hsl(0, 100%, 66%)";
-        cvc.style.border = `1px solid hsl(0, 100%, 66%)`
-    }
+    // if (cvc.value === ""){
+    //     cDisplay.textContent = `Can't be blank`;
+    //     cDisplay.style.color = "hsl(0, 100%, 66%)";
+    //     cvc.style.border = `1px solid hsl(0, 100%, 66%)`
+    // }
 
-    else {
-        display.textContent = ``;
-        pMessage.textContent = ``;
-        dMessage.textContent = ``;
-    }
+    // else {
+    //     display.textContent = ``;
+    //     pMessage.textContent = ``;
+    //     dMessage.textContent = ``;
+    //     contiune.classList = "disp"
+    //     complete.classList = ``
+    // }
+
+    contiune.classList = "disp"
+    complete.classList = ``
 
     
 
 })
-
-submit.onclick = () => {
-    complete.classList.add("comp");
-
-}
 
